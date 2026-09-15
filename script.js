2e4e419bebde975dfbb9e31c87bb55d7a90cfb03
carnocentaurus <carnocentaurus@gmail.com>
2026-09-15 09:42:31 +0800

    Add addBookToLibrary function with initial logic

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const library = [];
 
-function Book(title, author, pages, read) {
+function Book(id, title, author, pages, read) {
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
+
+function addBookToLibrary() {
+    const bookId = crypto.randomUUID();
+
+    const newBook = new Book(
+        bookId, 
+        'One Piece', 
+        'Eichiiro Oda', 
+        200, 
+        true
+    );
+
+    library.push(newBook);
+}
+
+addBookToLibrary();
