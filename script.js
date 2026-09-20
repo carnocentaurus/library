97cd3d9ab64ad57e46ebfaae29383bb7d33de4ef
carnocentaurus <carnocentaurus@gmail.com>
2026-09-20 16:58:25 +0800

    Replace alert() error messages with custom errorMessage element

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const errorMessage = document.querySelector('.error-message');
 const bookTitleInput = document.querySelector('#book-title-input');
 const bookAuthorInput = document.querySelector('#book-author-input');
 const bookPagesInput = document.querySelector('#book-pages-input');
@@ -101,37 +102,38 @@ addBookButton.addEventListener('click', (event) => {
     );
 
     if (bookTitleInput.value.length > 150) {
-        alert("Book title can't be more than 150 characters!");
+        errorMessage.textContent = "Book title can't be more than 150 characters";
         return;
     }
     if (bookTitleInput.value.trim() === '') {
-        alert('Please enter book title!');
+        errorMessage.textContent = 'Enter book title';
         return;
     }
     if (bookAuthorInput.value.length > 70) {
-        alert("Book author can't be more than 70 characters!");
+        errorMessage.textContent = "Book author can't be more than 70 characters";
         return;
     }
     if (bookAuthorInput.value.trim() === '') {
-        alert('Please enter book author!');
+        errorMessage.textContent = 'Enter book author';
         return;
     }
     if (bookPagesInput.value.trim() === '') {
-        alert('Please enter number of pages!');
+        errorMessage.textContent = 'Enter number of pages';
         return;
     }
     if (parseInt(bookPagesInput.value) < 1) {
-        alert('Book must have at least one page!');
+        errorMessage.textContent = 'Book must have at least one page';
         return false;
     }
     if (parseInt(bookPagesInput.value) > 10000) {
-        alert("Number of pages can't be more than 10,000!");
+        errorMessage.textContent = "Number of pages can't be more than 10,000";
         return;
     }
 
     library.push(book);
 
-    alert('Book added to library');
+    alert('Book added to library!');
+    errorMessage.textContent = '';
 
     showLibrary();
 });
